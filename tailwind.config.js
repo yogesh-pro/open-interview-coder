1d41d38cc42861ed7c4e3bc8867fb28a754f38ab
Hoan Tran <xhoantran@gmail.com>
2025-03-29 19:09:17 -0400

    fix: clean up tailwind config js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,7 @@
-const colors = require('tailwindcss/colors');
-
 module.exports = {
   content: ['./src/renderer/**/*.{js,jsx,ts,tsx,ejs}'],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        sky: colors.sky,
-        cyan: colors.cyan,
-      },
-    },
-  },
+  theme: {},
   variants: {
     extend: {},
   },
