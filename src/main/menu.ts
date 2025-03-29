ee876611e1584da5ab75ecad1cfcd97bb1d7c09e
Hoan Tran <xhoantran@gmail.com>
2025-03-29 18:13:27 -0400

    update package json metadata

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -54,17 +54,17 @@ export default class MenuBuilder {
 
   buildDarwinTemplate(): MenuItemConstructorOptions[] {
     const subMenuAbout: DarwinMenuItemConstructorOptions = {
-      label: 'Electron',
+      label: 'Open Interview Coder',
       submenu: [
         {
-          label: 'About ElectronReact',
+          label: 'About Open Interview Coder',
           selector: 'orderFrontStandardAboutPanel:',
         },
         { type: 'separator' },
         { label: 'Services', submenu: [] },
         { type: 'separator' },
         {
-          label: 'Hide ElectronReact',
+          label: 'Hide Open Interview Coder',
           accelerator: 'Command+H',
           selector: 'hide:',
         },
